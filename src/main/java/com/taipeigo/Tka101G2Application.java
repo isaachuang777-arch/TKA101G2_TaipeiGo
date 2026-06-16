f7b3add12b2b5ab7068c510ac3cf4d2389a61156
Isaac <Isaachuang777@gmail.com>
2026-06-16 16:54:17 +0800

    first commit

---
+++ src/main/java/com/taipeigo/Tka101G2Application.java
@@ -0,0 +1,13 @@
+package com.taipeigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tka101G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tka101G2Application.class, args);
+	}
+
+}
