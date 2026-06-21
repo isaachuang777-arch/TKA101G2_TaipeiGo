fc1eae1ead7d03c40cdbd21716f2c8daf0099af7
theell3 <the.ell3.com@gmail.com>
2026-06-22 00:33:38 +0800

    門票分類改為軟刪除

--- src/main/java/com/taipeigo/ticketcategory/model/TicketCategoryRepository.java
+++ src/main/java/com/taipeigo/ticketcategory/model/TicketCategoryRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TicketCategoryRepository extends JpaRepository<TicketCategoryVO, Integer> {
-	
-	// （自訂）條件查詢：只撈出特定啟用狀態的門票分類
+
+	// （自訂）條件查詢：只撈出啟用(ticketCategoryStatus = 1)狀態的門票分類
 	@Query(value = "from TicketCategoryVO where ticketCategoryStatus = ?1")
-    List<TicketCategoryVO> findByTicketCategoryStatus(Integer ticketCategoryStatus);
+	List<TicketCategoryVO> findByTicketCategoryStatus(Integer ticketCategoryStatus);
+
+	// 撈出所有未被刪除的分類 (ticketCategoryStatus !=2 )
+	@Query(value = "from TicketCategoryVO where ticketCategoryStatus != 2 order by ticketCategoryId asc")
+	List<TicketCategoryVO> findAllNotDeleted();
 
 }
-
