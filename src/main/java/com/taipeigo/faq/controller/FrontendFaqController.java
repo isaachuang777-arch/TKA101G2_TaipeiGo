9482d1c2483e6830643c5f70bdc8c7fdb01bc09b
miyu_ngai <miyu.ngai@outlook.com>
2026-06-25 15:29:05 +0800

    新增前台FAQ VO,repository,frontendcontroller,頁面

---
+++ src/main/java/com/taipeigo/faq/controller/FrontendFaqController.java
@@ -0,0 +1,34 @@
+package com.taipeigo.faq.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.taipeigo.faq.model.FaqRepository;
+import com.taipeigo.faq.model.FaqVO;
+
+@Controller
+@RequestMapping("/FAQ")
+public class FrontendFaqController {
+
+	@Autowired
+	private FaqRepository faqRepository;
+	
+@GetMapping({"/", "/index"})
+	public String showindexpage(Model model) {
+		List<FaqVO> faqVO = faqRepository.findAll();
+		model.addAttribute("faqVO", faqVO);
+		return "frontend/faq/index";
+}
+@GetMapping("/faq")
+	public String showfaqpage(Model model) {
+		List<FaqVO> faqVO = faqRepository.findAll();
+		model.addAttribute("faqVO", faqVO);
+		return "frontend/faq/faq";
+}
+
+}
