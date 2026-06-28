a67ac6be3d707b673ae6d74528930eee253bf9a4
theell3 <the.ell3.com@gmail.com>
2026-06-28 18:45:33 +0800

    後台門票分類加上分頁設定

--- src/main/java/com/taipeigo/ticketcategory/model/TicketCategoryService.java
+++ src/main/java/com/taipeigo/ticketcategory/model/TicketCategoryService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +14,18 @@ public class TicketCategoryService {
 
 	@Autowired
 	private TicketCategoryRepository repository;
+
+	// 找出所有未被刪除的 category (分頁)
+	public Page<TicketCategoryVO> getNotDeletedWithPage(int pageNumber) {
+		Pageable pageable = PageRequest.of(pageNumber, 10); // 一頁 10 筆
+		return repository.findAllNotDeleted(pageable);
+	}
+
+	// 模糊搜尋未被刪除的 category (分頁)
+	public Page<TicketCategoryVO> searchByName(String keyword, int pageNumber) {
+		Pageable pageable = PageRequest.of(pageNumber, 10); // 一頁 10 筆
+		return repository.findByName(keyword, pageable);
+	}
 
 	public void addTicketCategory(TicketCategoryVO ticketCateVO) {
 		repository.save(ticketCateVO);
@@ -45,7 +60,7 @@ public class TicketCategoryService {
 		return repository.findByTicketCategoryStatus(1);
 	}
 
-	// 找出所有未被刪除的 category
+	// 找出所有未被刪除的 category => 啟用 和 未啟用
 	public List<TicketCategoryVO> getAllNotDeleted() {
 		return repository.findAllNotDeleted();
 	}
