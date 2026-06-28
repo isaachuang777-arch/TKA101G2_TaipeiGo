c0bcccf2f037e139a96af71f1b5908f91a192afd
theell3 <the.ell3.com@gmail.com>
2026-06-28 23:54:28 +0800

    調整門票分類刪除設定

--- src/main/java/com/taipeigo/ticketcategory/model/TicketCategoryService.java
+++ src/main/java/com/taipeigo/ticketcategory/model/TicketCategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TicketCategoryService {
@@ -36,12 +37,16 @@ public class TicketCategoryService {
 		// 後續優化：加上安全檢查確認前端傳來的欄位資料都在
 	}
 
+	@Transactional
 	public void deleteTicketCategory(Integer ticketCateId) {
 		Optional<TicketCategoryVO> optional = repository.findById(ticketCateId);
 		if (optional.isPresent()) {
 			TicketCategoryVO vo = optional.get();
-			vo.setTicketCategoryStatus(2); // 代表軟刪除
+			vo.setTicketCategoryStatus(2); // 代表軟刪除 => db保留只是狀態改變
 			repository.save(vo);
+
+			// 同步刪除 TICKET_CATEGORY_INFO 中的關聯資料 （硬刪除 => db 真的刪除）
+			repository.deleteAssociationsByCategoryId(ticketCateId);
 		}
 	}
 
