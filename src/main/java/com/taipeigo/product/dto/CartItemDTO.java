7cbf217b0cd5d3a71f441871c0c4f7cf51c4d44f
Isaac <Isaachuang777@gmail.com>
2026-07-02 13:02:26 +0800

    修改部分typo

--- src/main/java/com/taipeigo/product/dto/CartItemDTO.java
+++ src/main/java/com/taipeigo/product/dto/CartItemDTO.java
@@ -18,7 +18,7 @@ public class CartItemDTO implements Serializable {
     
     private Integer subtotal;           // 小計 (單價 * 數量)
 
-    @JsonFormat(pattern = "yyy-MM-dd", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private LocalDateTime expiryDate;   // 票券到期日 (如果是活動，這個欄位就
     private String spec;                // 記錄票種用的 - 成人票兒童票之類的
 
